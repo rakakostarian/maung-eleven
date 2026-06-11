b4cfaf128d22f9153653cdbcf00af9ceb3379992
Raka Kostarian <raka@RAKA-ID-MAC-PYPP2.local>
2026-06-11 14:40:13 +0700

    add analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+\import React from 'react';
 import MaungEleven from './MaungEleven';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
-  return <MaungEleven />;
+  return (
+    <>
+      <MaungEleven />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
